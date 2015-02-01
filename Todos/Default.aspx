5cbe4710ff5fe804110548266e6336f8ad0c4e58
Exploseph <josephcnitti@gmail.com>
2015-02-01 15:56:42 -0600

    New Features - Add, drag and drop.  CSS updated.

--- Todos/Default.aspx
+++ Todos/Default.aspx
@@ -1,8 +1,27 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Todo.master" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
 
-<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="Server">
 </asp:Content>
-<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
- 
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="Server">
+    <a href="#" data-bind="click: addTodo">Add todo</a>
+    <div class="container" data-bind="sortable: todos">
+        <div class="row">
+            <div class="todo-row">
+                <div class="todo-container">
+                    <div class="drag-handle pull-left"></div>
+                    <div class="todo-checkbox pull-left">
+                        <input type="checkbox" data-bind="checked: is_complete, value: is_complete, event: { click: $root.itemChanged }" />
+                    </div>
+                    <div class="col-md-10 pull-left" data-bind="visible: !$root.isTodoSelected($data)">                        
+                        <a href="#" title="Click to edit" data-bind="html: description, click: $root.selectedTodo"></a>                        
+                        <a href="#" title="Click to edit" data-bind="visible: (description && !description()), click: $root.selectedTodo">edit</a>                        
+                    </div>
+                    <div class="col-md-10 pull-left" data-bind="visibleAndSelect: $root.isTodoSelected($data)">
+                        <input data-bind="value: description, event: { blur: $root.clearTodo, change: $root.itemChanged }" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </asp:Content>
 
--- Todos/Scripts/todo.js
+++ Todos/Scripts/todo.js
@@ -1,18 +1,57 @@
-﻿
-var CUC = 'cuc';
+﻿var CUC = 'cuc';
 var FORMID = "todosForm";
 var EMAIL_CTL = "email";
 var PASS_CTL = "password";
 var ERROR_CTL = "errorContainer";
 
-
 $(document).ready(function () {
-    ko.applyBindings(new AppViewModel(), $("#navbar")[0]);
+    ko.bindingHandlers.sortable.options.handle = ".drag-handle";
+    // Show hide edit
+    ko.bindingHandlers.visibleAndSelect = {
+        update: function (element, valueAccessor) {
+            ko.bindingHandlers.visible.update(element, valueAccessor);
+            if (valueAccessor()) {
+                setTimeout(function () {
+                    $(element).find("input").focus().select();
+                }, 0);
+            }
+        }
+    };
+
+    ko.applyBindings(new todoViewModel());
     setNavActive();
 });
 
-function AppViewModel() {
-    this.isUserLoggedIn = ko.observable(Application.isUserLoggedIn());
+function todoViewModel() {
+    var self = this;
+    self.isUserLoggedIn = ko.observable(Application.isUserLoggedIn());
+    self.todos = Application.usersTodos;
+    self.itemChanged = function (todo) {
+        Application.updateTodo(todo);
+        return true;
+    };
+    self.clearTodo = function (data, event) {
+        if (data === self.selectedTodo()) {
+            self.selectedTodo(null);
+        }
+    };
+    self.selectedTodo = Application.selectedTodo;
+    self.selectTodo = function (todo) {
+        self.selectedTodo = todo;
+    };
+    self.isTodoSelected = function (todo) {
+        return todo === self.selectedTodo();
+    };
+    self.addTodo = function () {
+        var todo = new observableTodo(null, "", false);
+        Application.addTodo(todo);
+    }
+}
+
+function observableTodo(id, description, is_complete) {
+    this.id = id;
+    this.description = ko.observable(description);
+    this.is_complete = ko.observable(is_complete);
 }
 
 function setNavActive() {
@@ -24,7 +63,11 @@ function setNavActive() {
 
 Application = {
     isUserLoggedIn: function () {
-        return (this._getUserContent() != null);
+        if (Todo.USER == null && this.getUserContent() != null) {
+            Todo.USER = this.getUserContent();
+            this.loadTodos();
+        }
+        return (this.getUserContent() != null);
     },
 
     createUser: function () {
@@ -87,11 +130,70 @@ Application = {
         $.cookie(CUC, JSON.stringify(data), { path: "/" });
     },
 
-    _getUserContent: function () {
+    getUserContent: function () {
         return ($.cookie(CUC)) ? JSON.parse($.cookie(CUC)) : null;
+    },
+
+    loadTodos: function () {
+        var options = {
+            success: function (data) {
+                $.each(data, function (i, o) { Application.usersTodos.push(new observableTodo(o.id, o.description, o.is_complete)); });
+            },
+            error: function (data) {
+                alert('Error getting todos');
+            }
+        };
+
+        Todo.loadTodos(options);
+    },
+
+    selectedTodo: ko.observable(),
+
+    usersTodos: ko.observableArray(),
+
+    addTodo: function (todo) {
+        var options = {
+            success: function (newTodo) {
+                var obTodo = new observableTodo(newTodo.id, newTodo.description, newTodo.is_complete);
+                Application.usersTodos.splice(0, 0, obTodo);
+                Application.selectedTodo(obTodo);
+            },
+            error: function () {
+                alert('Error adding todo');
+            },
+            todo: {
+                "description": todo.description(),
+                "is_complete": todo.is_complete()
+            }
+        };
+
+        Todo.createTodo(options);
+    },
+
+    updateTodo: function (todo) {
+        if (todo.id == null) {
+            this.addTodo(todo);
+        }
+        else {
+            var options = {
+                data: {
+                    description: todo.description,
+                    is_complete: todo.is_complete
+                },
+                todoId: todo.id,
+                success: function (t) {
+
+                },
+                error: function () {
+                    alert("Update todo error");
+                }
+            };
+
+            Todo.updateTodo(options);
+        }
     },
 
     _pageIsValid: function () {
         return $("#" + FORMID).valid();
-    }    
+    }
 }
